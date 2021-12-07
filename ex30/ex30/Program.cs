9477e34c31181ac14fdeb30d3f3ffa6f97f8fca1
Jean Moraes <jeanmoraes00@gmail.com>
2021-12-07 03:37:12 -0300

    exercicio concluido

---
+++ ex30/ex30/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ex30.Entities;
+
+namespace ex30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "alex@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+        }
+    }
+}
