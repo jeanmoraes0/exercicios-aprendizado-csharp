9b990bf79d125162465e4eecde8d4eeff99cf3d0
Jean Moraes <jeanmoraes00@gmail.com>
2021-12-08 02:22:20 -0300

    exercicio concluido

---
+++ ex31/ex31/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using ex31.Entities;
+
+namespace ex31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            Console.ReadLine();
+            string path = "in.txt";
+
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { Username = name, Instant = instant });
+                    }
+
+                    Console.WriteLine("Total users: " + set.Count);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
